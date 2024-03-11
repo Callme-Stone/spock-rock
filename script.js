bce50744d64507ff02621b21988687100a728d76
Callme-Stone <abolajioluwaseyi0@gmail.com>
2024-03-11 15:12:45 +0100

    first commit

--- script.js
+++ script.js
@@ -63,6 +63,18 @@ function updateScores (playerChoice) {
     console.log(playerChoice, computerChoice)
     if (playerChoice === computerChoice) {
         resultText.textContent = "It's a tie"
+    }else {
+        const choice = choices[playerChoice]
+        console.log(choice.defeats.indexOf(computerChoice))
+        if(choice.defeats.indexOf(computerChoice) > -1){
+            resultText.textContent = 'You Won!!!'
+            playerScoreNumber++
+            playerScoreEl.textContent = playerScoreNumber
+        }else{
+            resultText.textContent = 'You Lost!'
+            computerScoreNumber++
+            computerScoreEl.textContent = computerScoreNumber
+        }
     }
 }
 
